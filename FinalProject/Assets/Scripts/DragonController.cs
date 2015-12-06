5c5ad7c4b4e7892b7fd16a6d23ad3e5e6f4f3cb9
wang-eric <ericwang1988@hotmail.com>
2015-12-05 21:43:07 -0500

    Ver.0.6.2 Level 3 Monster Added
    - Added Monster animation and script for level 3

---
+++ FinalProject/Assets/Scripts/DragonController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragonController : MonoBehaviour {
+	// PUBLIC INSTANCE VARIABLES
+	public float speed = 0.01f;
+	public bool fire = false;
+	private Transform _playerTransform;
+	private SimplePlatformController _playerController;
+	private Animator anim;
+	// PRIVATE INSTANCE VARIABLES
+	private Transform _transform;
+	
+	public int Range = 20;
+	private int counter = 0;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		this._transform = gameObject.GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (counter == Range){
+			this.anim.SetBool ("fire", true);
+			speed=0;
+			counter = 0;
+		} else {
+			this.anim.SetBool ("fire", false);
+			speed = 0.01f;
+		}
+		this._transform.position += new Vector3(speed,0,0);
+		counter += 1;
+	}
+}
