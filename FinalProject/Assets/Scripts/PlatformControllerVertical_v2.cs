7ea6b61f8b8006a83f802c690c9b0fe91bdb7407
wang-eric <ericwang1988@hotmail.com>
2015-12-13 12:44:37 -0500

    Ver.0.9.2a Pause Function Bug Fix
    - Fixed a bug where the moving platforms were still moving when the game
    was paused.

---
+++ FinalProject/Assets/Scripts/PlatformControllerVertical_v2.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformControllerVertical_v2 : MonoBehaviour {
+	// PUBLIC INSTANCE VARIABLES
+	public float speed = 1f;
+	public float timer = 0f;
+	public float timimg = 4f;
+
+	// PRIVATE INSTANCE VARIABLES
+	private Rigidbody2D rb2d;
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer > timimg) {
+			timer = 0;
+			speed = -speed;
+		}
+		rb2d.velocity = new Vector2 (rb2d.velocity.x, speed);
+		/*
+		if (counter == moveRange){
+			moveUp = !moveUp;
+		}
+
+		if (moveUp) {
+			rb2d.velocity = new Vector2 (speed, rb2d.velocity.y);
+			this._transform.position += new Vector3(0,speed,0);
+		} else {
+			rb2d.velocity = new Vector2 (-speed, rb2d.velocity.y);
+			this._transform.position -= new Vector3(0,speed,0);
+		}*/
+	}
+}
